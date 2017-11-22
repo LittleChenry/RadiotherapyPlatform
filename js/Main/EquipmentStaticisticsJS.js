165ed0e86b652568d795ab6fc774e7e30931a04b
peach <405891205@qq.com>
2017-11-22 17:03:19 +0800

    设备月统计

---
+++ js/Main/EquipmentStaticisticsJS.js
@@ -0,0 +1,99 @@
+﻿/**
+ *
+ **/
+
+/**
+ * 生成设备选择菜单下拉选项
+ */
+$(function () {
+    getAllEquipment();
+});
+
+/**
+ * 获取所有设备
+ */
+function getAllEquipment() {
+    $.ajax({
+        type: "post",
+        url: "/RadiotherapyPlatform/pages/Main/GetSessionEquipment.ashx",
+        dataType: "text",
+        success: function (data) {
+            fillSelect($.parseJSON(data));
+        }
+    })
+}
+
+/**
+ * 填充下拉菜单
+ */
+function fillSelect(obj) {
+    var $select = $("#equipmentSelect");
+    if(obj.length != undefined && obj.length != null)
+        for (var i = 0; i < obj.length; ++i) {
+            $select.append("<option value=" + obj[i].ID + ">" + obj[i].Name + "</option>");
+        }
+    else
+        $select.append("<option value=" + obj.ID + ">" + obj.Name + "</option>");
+}
+
+/**
+ * 搜索按钮事件
+ */
+$(function () {
+    $("#search").bind("click", function () {
+        var $equipment = $("#equipmentSelect").find(":selected");
+        var dates = $("#dates").val();
+        $.ajax({
+            type: "post",
+            url: "/RadiotherapyPlatform/pages/Root/getCheckRecord.ashx",
+            data: { "equipmentID": $equipment.val(), "date": dates },
+            dataType: "text",
+            success: function (data) {
+                if (data == "null") {
+                    return;
+                }
+                var jsonObj = $.parseJSON(data);
+                var max = 0, index = 0;
+                for (var i = 0; i < jsonObj.length; ++i) {
+                    var len = countLength(jsonObj[i]);
+                    if (max < len) {
+                        max = len;
+                        index = i;
+                    }
+                }
+                var head = new Array();
+                for (i in jsonObj[index]) {
+                    head.push(i);
+                }
+                $("#tableArea").createTable(jsonObj, {
+                    rows: 12,
+                    needDate: true,
+                    createDate: dates,
+                    lessLength: max,
+                    headName: head
+                });
+                $.ajax({
+                    type: "post",
+                    url: "/RadiotherapyPlatform/pages/Main/getMainItemLength.ashx",
+                    data: { "equipmentID": $equipment.val() },
+                    success: function (MainItem) {
+                        var jsonObj = $.parseJSON(MainItem);
+                        var MainTr = new Array();
+                        MainTr.push("<tr><th> </th>");
+                        for (var i = 0; i < jsonObj.length; i++) {
+                            MainTr.push("<th style='border-right:1px solid #ccc;border-left:1px solid #ccc;' colspan=" + jsonObj[i].len + ">" + jsonObj[i].name + "</th>");
+                        }
+                        var trMain = MainTr.join('');
+                        $("#tableArea thead").prepend(trMain);
+                    }
+                });
+            }
+        })
+    });
+});
+
+function countLength(obj) {
+    var arr = Object.keys(obj);
+    var len = arr.length;
+    return len;
+}
